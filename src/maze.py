9dc7849bb524fb874ed46472d46dd6a5660f1f92
ricebiceps <ricebiceps@gmail.com>
2024-06-07 23:38:58 -0400

    wrote tests for maze, updated gitignore to include pycache directory

--- src/maze.py
+++ src/maze.py
@@ -10,7 +10,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win=None,
     ):
         self._cells = []
         self._x1 = x1
@@ -40,6 +40,8 @@ class Maze:
         self._animate()
     
     def _animate(self):
+        if self._win is None:
+            return
         self._win.redraw()
         time.sleep(0.05)
 
